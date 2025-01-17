95e233c0182ef45418351b014c0d6d15bedd41d8
Emam Uddin <emamskingdom@gmail.com>
2025-01-17 23:20:55 +0600

    added compo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@
 const Header = () => {
     return (
         <div>
-           <h1 className='text-3xl bg-red-400'>Vite + React</h1> 
+           <h1 className='text-3xl bg-red-400'>Vite + </h1> 
         </div>
     );
 };
