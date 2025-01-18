ca9f31c9ca8d8163743300a94b8247b23713fdf3
Emam Uddin <emamskingdom@gmail.com>
2025-01-18 16:18:01 +0600

    added state

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,9 +1,21 @@
+import { useEffect } from "react";
+import { useState } from "react";
 
 
 const Blogs = () => {
+    const [blogs,setBlogs] = useState([])
+    console.log(Blogs)
+    useEffect(()=>{
+        fetch('blogs.json')
+        .then(res => res.json())
+        .then(data => setBlogs(data))
+    },[])
     return (
-        <div>
+        <div className="bg-red-400">
             <h3 className="text-black">the blogs is connected</h3>
+            {
+              blogs.map(blog => )
+            }
         </div>
     );
 };
