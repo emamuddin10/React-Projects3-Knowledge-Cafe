70a79f23f8f9e1f458b78b81c2947dab61ed4be9
Emam Uddin <emamskingdom@gmail.com>
2025-01-18 22:32:56 +0600

    added lenth

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -11,8 +11,8 @@ const Blogs = () => {
         .then(data => setBlogs(data))
     },[])
     return (
-        <div className="w-2/3">
-            <h3 className="text-black">Blogs</h3>
+        <div className="md:w-2/3">
+            <h3 className="text-black">Blog Lenth: {blogs.length}</h3>
             {/* {
               blogs.map(blog => )
             } */}
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,12 +2,12 @@
 import profile from '../../assets/profile.png'
 const Header = () => {
     return (
-        <div className='flex justify-between container mx-auto py-5 border-b-2'>
+        <header className='flex justify-between container mx-auto py-5 border-b-2'>
            <h1 className='text-3xl'>Knowledge Cafe</h1>
            <img className='w-[50px]' src={profile} alt="" />
            
            
-        </div>
+        </header>
     );
 };
 
