d39eaeb70ffae1f6dc1ae33ca31c1ad43074d223
Emam Uddin <emamskingdom@gmail.com>
2025-01-19 21:51:29 +0600

    create bookmark page and get bookmard data

--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -1,11 +1,25 @@
 
+import PropTypes from "prop-types";
+import Bookmark from "../Bookmark/Bookmark";
 
-const Bookmarks = () => {
-    return (
-        <div className="w-1/3">
-            <h2>Bookmarks:</h2>
-        </div>
-    );
+const Bookmarks = ({bookmarks,readingTime}) => {
+    console.log(bookmarks)
+  return (
+    <div className="w-1/3 p-10 bg-slate-400 mt-8 ml-10">
+       <h1>Spend Time On Read:{readingTime}</h1>
+      <h2>Bookmarks Length:{bookmarks.length}</h2>
+
+      {
+        bookmarks.map((bookmark,idx) =>(
+          <Bookmark key={idx} bookmark={bookmark}></Bookmark>
+        ))
+      }
+    </div>
+  );
+};
+
+Bookmarks.propTypes = {
+    bookmarks: PropTypes.array
 };
 
 export default Bookmarks;
