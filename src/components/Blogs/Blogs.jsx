7d78e8b0343541f038ea590bd0c4c20c0b3eea57
Emam Uddin <emamskingdom@gmail.com>
2025-01-19 21:50:52 +0600

    create blog pages and bookmard css file

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,10 +1,12 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import Blog from "../Blog/Blog";
+import PropTypes from "prop-types";
 
 
-const Blogs = () => {
+const Blogs = ({handleAddToBookmark,handleSetAsTime}) => {
     const [blogs,setBlogs] = useState([])
-    console.log(Blogs)
+    // console.log(Blogs)
     useEffect(()=>{
         fetch('blogs.json')
         .then(res => res.json())
@@ -13,11 +15,19 @@ const Blogs = () => {
     return (
         <div className="md:w-2/3">
             <h3 className="text-black">Blog Lenth: {blogs.length}</h3>
-            {/* {
-              blogs.map(blog => )
-            } */}
+            {
+              blogs.map(blog =><Blog 
+                key={blog.id} 
+                handleSetAsTime={handleSetAsTime}
+                handleAddToBookmark={handleAddToBookmark}
+                blog={blog}></Blog> )
+            }
         </div>
     );
 };
+
+Blogs.prototype={
+    handleAddToBookmark: PropTypes.object
+}
 
 export default Blogs;
